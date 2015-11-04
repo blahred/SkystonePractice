6a09485a1732862129820fba4305ca2ffcc3084b
Kevin Wong <kgwong1@uci.edu>
2015-11-03 23:54:07 -0800

    Update main.cpp

--- Cpp_Git_Practice/main.cpp
+++ Cpp_Git_Practice/main.cpp
@@ -12,13 +12,16 @@
 
 //***YOUR CLASS INCLUDES HERE***
 
+#define FULL_PARTY_BONUS 722
+
 int main(int argc, char** argv)
 {
 	PillarOfDoom pillar(1000);
 
 	std::vector<Programmer*> programmers;
 
-	//add a new'd instance (via pointer) of your class to the vector here 
+	//add a new'd instance (via pointer) of your class to the vector here. Use vector.push_back()
+
 
 
 
@@ -31,6 +34,12 @@ int main(int argc, char** argv)
 		std::cout << "Pillar has " << pillar.getHealth() << " health left!" << std::endl;
 	}
 	std::cin.ignore();
+	if (programmers.size() == 5)
+	{
+		pillar.damage(FULL_PARTY_BONUS);
+		std::cout << "Pillar has " << pillar.getHealth() << " health left!" << std::endl;
+		std::cout << "Pillar defeated" << std::endl;
+	}
 
 	//delete your new'd instance here
 
@@ -39,7 +48,6 @@ int main(int argc, char** argv)
 
 
 	//delete your new'd instance here
-
 
 	return 0;
 }
